188d98a52a20a9ff3e06a9d36c56028d002299ad
AmitPrasad212003 <amitprasad212003@gmail.com>
2026-02-28 01:01:03 +0530

    componets

--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+ import { useEffect, useRef } from "react";
 import { useGLTF, useAnimations } from "@react-three/drei";
 
 import planeScene from "../assets/3d/plane.glb";
